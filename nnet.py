7a3aa9014cb2b95eb9ebd513f2c18b91a9f4182b
soyeon Park <sypark9646@gmail.com>
2020-02-07 10:36:07 +0900

    add checkpoint and early stopping

--- nnet.py
+++ nnet.py
@@ -23,69 +23,292 @@
 
 import torch
 from torch.utils.data import DataLoader
-
 from data import DeWatermarkerDataset
-from autoencoder import ARCH0Autoencoder, ARCH1Autoencoder, ARCH2Autoencoder
 from utils import display 
+import torch.nn as nn
 
 
-# Hyperparameters.
-BATCH_SIZE = 4
-SHUFFLE = True
-NUM_WORKERS = 4
-N_EPOCHS = 2000
-N_BATCHES = 10
-ETA = 1e-3
+class BaseAutoencoder(nn.Module):
+    """
+    Provides some common functionality across the different autoencoder
+    model architectures.
+    """
+
+    def forward(self, x):
+        """
+        Perform the forward pass on the given input.
+        Args:
+            x (Tensor): The input to perform the forward pass on.
+        """
+        x = self.resize(x)
+        encoded_x = self.encoder(x)
+        decoded_x = self.decoder(encoded_x)
+
+        return decoded_x
+
+    def load(self):
+        """
+        Load in any existing weights belonging to this model.
+        """
+        optimizer = torch.optim.Adam(
+            model.parameters(),
+            lr=ETA,
+            weight_decay=1e-5
+        )
+        loss = None
+        try:
+            checkpoint = torch.load(self.FPATH)
+            if isinstance(checkpoint, dict) and 'model_state_dict' in checkpoint:
+                self.load_state_dict(checkpoint['model_state_dict'])
+            else:
+                self.load_state_dict(checkpoint)
+                
+            if isinstance(checkpoint, dict) and 'optimizer_state_dict' in checkpoint:
+                optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+                
+            if isinstance(checkpoint, dict) and 'epoch' in checkpoint:    
+                loss = checkpoint['loss']
+
+            #self.load_state_dict(torch.load(self.FPATH))
+            self.eval()
+            return optimizer, loss
+            
+        except FileNotFoundError:
+            msg = "No existing model to initialize from. Creating new one ..."
+            print(msg)
+
+    def save(self, optimizer, loss):
+        """
+        Save the current state of this model.
+        """
+        torch.save({
+        'model_state_dict': self.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss,
+        }, self.FPATH)
+
+    def resize(self, sample):
+        """
+        Resize a sample so that it can be inputted to a PyTorch
+        Conv2D layer. We need to do this, as PyTorch our input
+        is expected in the following shape:
+            (batch_size, n_channels, height, width)
+        """
+        return sample.permute(0, 3, 1, 2).type("torch.FloatTensor")
 
 
-# Data setup.
-dataset = DeWatermarkerDataset(root_dir="data/training/set.pkl")
-INPT_SHAPE = dataset[0]["watermarked"].shape
-dataloader = DataLoader(
-    dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=SHUFFLE,
-    num_workers=NUM_WORKERS
-)
+class ARCH0Autoencoder(BaseAutoencoder):
+    """
+    First autoencoder architecture.
+    We'll go with a shallow 1-layer convolutional autoencoder.
+    """
+    KERNEL_SIZE = 3
+    STRIDE = 1
+    FPATH = "arch_0.pt"
+
+    def __init__(self, inpt_shape):
+        super().__init__()
+        _, _, inpt_channels = inpt_shape
+        self.encoder = nn.Sequential(
+            nn.Conv2d(
+                in_channels=inpt_channels,
+                out_channels=6,
+                kernel_size=self.KERNEL_SIZE,
+                stride=self.STRIDE
+            ),
+            nn.ReLU(inplace=True)
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=6,
+                out_channels=3,
+                kernel_size=self.KERNEL_SIZE
+            ),
+            nn.ReLU(inplace=True)
+        )
 
 
-# Model setup. Note, we have the option to load in an existing model.
-model = ARCH1Autoencoder(inpt_shape=INPT_SHAPE)
-model.load()
+class ARCH1Autoencoder(BaseAutoencoder):
+    """
+    Second autoencoder architecture. This will be a 2-layer convolutional
+    autoencoder model.
+    """
+    KERNEL_SIZE = 3
+    STRIDE = 1
+    FPATH = "arch_1.pt"
 
-# Loss function & optimizer setup.
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(
-    model.parameters(),
-    lr=ETA,
-    weight_decay=1e-5
-)
+    def __init__(self, inpt_shape):
+        super().__init__()
+        _, _, inpt_channels = inpt_shape
+        self.encoder = nn.Sequential(
+            nn.Conv2d(
+                in_channels=inpt_channels,
+                out_channels=6,
+                kernel_size=self.KERNEL_SIZE,
+                stride=self.STRIDE
+            ),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels=6,
+                out_channels=12,
+                kernel_size=self.KERNEL_SIZE,
+                stride=self.STRIDE
+            ),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels=12,
+                out_channels=24,
+                kernel_size=self.KERNEL_SIZE,
+                stride=self.STRIDE
+            ),
+            nn.ReLU(inplace=True)
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=24,
+                out_channels=12,
+                kernel_size=self.KERNEL_SIZE
+            ),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(
+                in_channels=12,
+                out_channels=6,
+                kernel_size=self.KERNEL_SIZE
+            ),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(
+                in_channels=6,
+                out_channels=inpt_channels,
+                kernel_size=self.KERNEL_SIZE
+            ),
+            nn.ReLU(inplace=True)
+        )
 
 
-# Train.
-# TODO: Make this a method on the Autoencoder class.
-min_loss = float("inf")
-for epoch in range(N_EPOCHS):
-    for i_batch, sample_batched in enumerate(dataloader):
-        watermarked = sample_batched["watermarked"]
-        original = sample_batched["original"]
-        original = model.resize(sample=original)
-        # NOTE: This resizing operation is __inverting__ the colours.
-        # It's fine given that our task is still to remove watermarks, but
-        # we'll want to fix this going forwards.
+class ARCH2Autoencoder(BaseAutoencoder):
+    """
+    Second autoencoder architecture. This will be a 2-layer convolutional
+    autoencoder model.
+    """
+    KERNEL_SIZE = 3
+    STRIDE = 1
+    FPATH = "arch_2.pt"
 
-        output = model(x=watermarked)
-        loss = criterion(output, original)
+    def __init__(self, inpt_shape):
+        super().__init__()
+        _, _, inpt_channels = inpt_shape
+        self.encoder = nn.Sequential(
+            nn.Conv2d(
+                in_channels=inpt_channels,
+                out_channels=64,
+                kernel_size=self.KERNEL_SIZE,
+                stride=self.STRIDE
+            ),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=self.KERNEL_SIZE,
+                stride=self.STRIDE
+            ),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=256,
+                kernel_size=self.KERNEL_SIZE,
+                stride=self.STRIDE
+            ),
+            nn.ReLU(inplace=True)
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=256,
+                out_channels=128,
+                kernel_size=self.KERNEL_SIZE
+            ),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(
+                in_channels=128,
+                out_channels=64,
+                kernel_size=self.KERNEL_SIZE
+            ),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(
+                in_channels=64,
+                out_channels=inpt_channels,
+                kernel_size=self.KERNEL_SIZE
+            ),
+            nn.ReLU(inplace=True)
+        )
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
 
-    # TODO: Move this into `debug()` method.
-    if epoch == 0 or epoch % 100 == 0:
-        print(">> epoch # {}: {}".format(epoch, loss.data))
-        display(output)
-        if loss < min_loss:
-            print(">> updating weights.")
-            model.save()
-            min_loss = loss
+
+if __name__ == '__main__':
+    torch.multiprocessing.freeze_support()
+    
+    # Hyperparameters.
+    BATCH_SIZE = 4
+    SHUFFLE = True
+    NUM_WORKERS = 4
+    N_EPOCHS = 2000
+    N_BATCHES = 10
+    ETA = 1e-3
+
+
+    # Data setup.
+    dataset = DeWatermarkerDataset(root_dir="data/training/set.pkl")
+    INPT_SHAPE = dataset[0]["watermarked"].shape
+    dataloader = DataLoader(
+        dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=SHUFFLE,
+        num_workers=NUM_WORKERS
+    )
+
+
+    # Model setup. Note, we have the option to load in an existing model.
+    model = ARCH1Autoencoder(inpt_shape=INPT_SHAPE)
+    saved_optimizer, saved_loss = model.load()
+
+    # Loss function & optimizer setup.
+    criterion = torch.nn.MSELoss()
+    optimizer = saved_optimizer
+    if optimizer is None:
+        optimizer = torch.optim.Adam(
+            model.parameters(),
+            lr=ETA,
+            weight_decay=1e-5
+        )
+    
+
+    # Train.
+    # TODO: Make this a method on the Autoencoder class.
+    min_loss = saved_loss
+    if min_loss is None:
+        min_loss = float("inf")
+
+    for epoch in range(N_EPOCHS):
+        for i_batch, sample_batched in enumerate(dataloader):
+            watermarked = sample_batched["watermarked"]
+            original = sample_batched["original"]
+            original = model.resize(sample=original)
+            # NOTE: This resizing operation is __inverting__ the colours.
+            # It's fine given that our task is still to remove watermarks, but
+            # we'll want to fix this going forwards.
+
+            output = model(x=watermarked)
+            loss = criterion(output, original)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # TODO: Move this into `debug()` method.
+        if epoch == 0 or epoch % 100 == 0:
+            print(">> epoch # {}: {}".format(epoch, loss.data))
+            display(output)
+            if loss < min_loss:
+                print(">> updating weights.")
+                model.save(optimizer=optimizer, loss=loss)
+                min_loss = loss
