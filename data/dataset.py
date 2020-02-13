bd9f8fba90136f8fb6f38b4d1cf97436df9de999
soyeon Park <sypark9646@gmail.com>
2020-02-13 14:15:07 +0900

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -30,8 +30,8 @@ class DeWatermarkerDataset(Dataset):
     """
     DeWatermarker project dataset.
     """
-
-    def __init__(self, root_dir, transform=None):
+    #참고 https://wingnim.tistory.com/33
+    def __init__(self, root_dir, transform=None): #download, read data 등등을 하는 파트
         """
         Args:
             root_dir ()
@@ -40,21 +40,42 @@ class DeWatermarkerDataset(Dataset):
         """
         self.root_dir = root_dir
         self.transform = transform
-        with open(root_dir, "rb") as fp:
-            self.dewatermarker_frame = cPickle.load(fp)
 
-    def __len__(self):
-        return len(self.dewatermarker_frame)
+        watermarked_data = []
+        original_data = []
 
-    def __getitem__(self, index):
+        path_of_wimages = os.path.join(root_dir, '/watermarked')
+        path_of_oimages = os.path.join(root_dir, '/original')
+
+        list_of_images = os.listdir(path_of_wimages)
+        for image in list_of_images:
+            img = cv2.imread(os.path.join(path_of_wimages, image), 0)
+            watermarked_data.append(img)
+            
+        list_of_images = os.listdir(path_of_oimages)
+        for image in list_of_images:
+            img = cv2.imread(os.path.join(path_of_oimages, image), 0)
+            original_data.append(img)
+
+        self.len = watermarked_data.shape[-1]
+        self.watermarked_data=watermarked_data
+        self.original_data=original_data
+
+    def __len__(self): #data size를 넘겨주는 파트
+        return self.len
+
+    def __getitem__(self, index): #인덱스에 해당하는 아이템을 넘겨주는 파트.
         """
         Get a sample from the dataset.
         Args:
             index (int): The index of the dataset element we're accessing.
         """
         # Get the sample, and apply any necessary transform (if any).
-        sample = self.dewatermarker_frame[index]
-        if self.transform:
-            sample = self.transform(sample)
+        sample_watermark = self.watermarked_data[index]
+        sample_original = self.original_data[index]
 
-        return sample
+        if self.transform:
+             sample_watermark = self.transform(sample_watermark)
+             sample_original = self.transform(sample_original)
+
+        return sample_watermark, sample_original
